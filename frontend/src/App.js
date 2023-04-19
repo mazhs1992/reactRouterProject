383d86b152efed0d2dea2a8993e2f87e5f0c4f81
Vangelis Mazis <mazhs1992@hotmail.com>
2023-04-19 12:15:33 +0300

    Reusable code for Form to be used for methods POST, PATCH

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from './pages/HomePage'
 import EventsPage, { loader as eventsLoader } from './pages/EventsPage'
 import EventDetailPage, { loader as eventDetailLoader, action as deleteEventAction } from './pages/EventDetailPage'
-import NewEventPage, { action as newEventAction } from './pages/NewEventPage'
+import NewEventPage from './pages/NewEventPage'
 import EditEventPage from './pages/EditEventPage'
 import RootLayout from "./pages/RootLayout";
 import ErrorPage from "./pages/Error";
 import EventsRootLayout from "./pages/EventsRoot";
+import { action as manipulateEventAction } from './components/EventForm'
 
 // Challenge / Exercise
 
@@ -63,12 +64,15 @@ const router = createBrowserRouter([
               },
               {
                 path: 'edit',
-                element: <EditEventPage />
+                element: <EditEventPage />,
+                action: manipulateEventAction
               },
             ]
           },
           {
-            path: 'new', element: <NewEventPage />, action: newEventAction
+            path: 'new',
+            element: <NewEventPage />,
+            action: manipulateEventAction
           },
         ]
       },
--- frontend/src/pages/NewEventPage.js
+++ frontend/src/pages/NewEventPage.js
@@ -2,38 +2,8 @@ import EventForm from "../components/EventForm"
 import { json, redirect } from "react-router-dom";
 
 function NewEventPage() {
-    return <EventForm />
+    return <EventForm method='post' />
 }
 
 export default NewEventPage
 
-export async function action({ request, params }) {
-
-    const data = await request.formData();
-    const eventData = {
-        title: data.get('title'),
-        image: data.get('image'),
-        date: data.get('date'),
-        description: data.get('description')
-    }
-
-    const responce = await fetch('http://localhost:8080/events/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(eventData)
-    })
-
-    if (responce.status === 422) {
-        return responce;
-    }
-    if (!responce.ok) {
-        throw json(
-            { message: 'Could not save event.' },
-            { status: 500 }
-        )
-    }
-
-    return redirect('/events')
-}
