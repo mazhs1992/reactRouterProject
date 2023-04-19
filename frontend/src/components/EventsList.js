cdf7e15a454ac15e1fbf5ebb2b2d8edcf3598a14
Vangelis Mazis <mazhs1992@hotmail.com>
2023-04-19 13:42:23 +0300

    control wchich data to defer

--- frontend/src/components/EventsList.js
+++ frontend/src/components/EventsList.js
@@ -2,6 +2,7 @@ import classes from './EventsList.module.css';
 import { Link } from 'react-router-dom';
 
 function EventsList({ events }) {
+
   console.log(events)
   return (
     <div className={classes.events}>
@@ -9,7 +10,7 @@ function EventsList({ events }) {
       <ul className={classes.list}>
         {events.map((event) => (
           <li key={event.id} className={classes.item}>
-            <Link to={event.id}>
+            <Link to={`/events/${event.id}`}>
               <img src={event.image} alt={event.title} />
               <div className={classes.content}>
                 <h2>{event.title}</h2>
--- frontend/src/pages/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage.js
@@ -1,14 +1,26 @@
-import { Link, useRouteLoaderData, useLoaderData, json, redirect } from "react-router-dom"
+import { Suspense } from "react"
+import { Link, useRouteLoaderData, useLoaderData, json, redirect, defer, Await } from "react-router-dom"
 import EventItem from '../components/EventItem'
+import EventsList from "../components/EventsList"
 
+function EventDetailPage() {
+    const { events, event } = useRouteLoaderData('event-detail')
 
-function EventDetailPage(props) {
-    const data = useRouteLoaderData('event-detail')
-    console.log(data)
 
     return (
         <>
-            <EventItem event={data.event} />
+            <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading ...</p>}>
+                <Await resolve={event}>
+                    {loadedEvent => <EventItem event={loadedEvent} />}
+                </Await>
+            </Suspense>
+
+            <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading ...</p>}>
+                <Await resolve={events}>
+                    {loadedEvents => <EventsList events={loadedEvents} />}
+                </Await>
+            </Suspense>
+
             <p><Link to='..' relative="path">Back</Link></p>
         </>
     )
@@ -16,9 +28,8 @@ function EventDetailPage(props) {
 
 export default EventDetailPage
 
-export async function loader({ request, params }) {
-
-    const id = params.eventsId;
+async function loadEvent(id) {
+    // const id = params.eventsId;
     const response = await fetch('http://localhost:8080/events/' + id)
 
     if (!response.ok) {
@@ -27,8 +38,39 @@ export async function loader({ request, params }) {
             { status: 500 }
         )
     } else {
-        return response;
+        const resData = await response.json()
+        return resData.event
     }
+}
+
+
+async function loadEvents() {
+    const response = await fetch('http://localhost:8080/events');
+    if (!response.ok) {
+        //return { isError: true, message: 'Could not fetch events.' }
+        //throw { message: 'Could not fetch events.' }
+        //throw new Response(JSON.stringify({ message: 'Could not fetch events.' }, { status: 500 }))
+        throw json(
+            { message: 'Could not fetch events.' },
+            { status: 500 }
+        )
+    } else {
+        const resData = await response.json()
+        console.log(resData.events)
+        return resData.events
+    }
+}
+
+export async function loader({ request, params }) {
+    const id = params.eventsId;
+
+    return defer({
+        event: await loadEvent(id),
+        events: loadEvents(),
+
+    })
+
+
 }
 
 export async function action({ request, params }) {
